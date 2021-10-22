8291607a4085e4f38332d3d4d32e0aff86bc644b
AkinoK <akashima@my.centennialcollege.ca>
2021-10-22 16:40:04 -0400

    Updated db URI to work with MongoDB Atlas

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
 
 module.exports = {
-    "URI": "mongodb://localhost/stock"
+    //"URI": "mongodb://localhost/stock"
+
+    "URI" : "mongodb+srv://akino22o:va1EOqNmh8rlQzjk@mongodbserver.vuv0t.mongodb.net/contacts?retryWrites=true&w=majority"
 }
